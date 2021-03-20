d60fbb29342e12816730ceda1c6a281d3a1ea9f2
surabhi-ttn <surabhi.chaurasia@tothenew.com>
2021-03-21 02:32:50 +0530

    Initial Commit

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,52 @@
+import React , {Component} from 'react';
+
+import PropTypes from 'prop-types';
+import classes from './BurgerIngredient.css';
+
+class BurgerIngredient extends Component {
+    render(){
+        let ingredient = null;
+
+        switch(this.props.type) {
+            case ('bread-bottom') :
+                ingredient = <div className={classes.BreadBottom}></div>
+                break;
+
+            case ('bread-top') :
+                ingredient = (
+                    <div className={classes.BreadTop}>
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
+                    </div>
+                );
+                break;
+            
+            case ('tikki') :
+                ingredient = <div className={classes.Tikki}></div>
+                break;
+                
+            case ('salad') :
+                ingredient = <div className={classes.Salad}></div>
+                break;
+
+            case ('cheese') :
+                ingredient = <div className={classes.Cheese}></div>
+                break;
+
+            case ('mayo') :
+                ingredient = <div className={classes.Mayo}></div>
+                break;
+
+            default :
+                ingredient = null;
+        }
+        return ingredient;
+    }
+    
+}
+
+BurgerIngredient.PropTypes = {
+    type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient
