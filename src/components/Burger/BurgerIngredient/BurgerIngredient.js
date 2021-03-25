d46945a182a5f68b6ae1577f210956c42b85fbd5
surabhi-ttn <surabhi.chaurasia@tothenew.com>
2021-03-25 14:34:25 +0530

    Added Form-Validation

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,6 @@
 import React , {Component} from 'react';
 
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
@@ -45,8 +45,8 @@ class BurgerIngredient extends Component {
     
 }
 
-BurgerIngredient.PropTypes = {
-    type: PropTypes.string.isRequired
+BurgerIngredient.propTypes = {
+    type: propTypes.string.isRequired
 };
 
 export default BurgerIngredient
